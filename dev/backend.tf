b2e8285a9805924340ce5ecd8d052465b514ee91
EC2 Default User <ec2-user@ip-172-31-28-101.eu-central-1.compute.internal>
2023-11-12 16:26:23 +0000

    Added Terraform work

--- dev/backend.tf
+++ dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "state-terraform-pragmatic-nikolay"
+    bucket         = "state-terraform-pragmatic-nikolay1"
     key            = "dev/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
